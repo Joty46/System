6d9d0a57fc342afe4415de1266da201299159e65
Joty46 <joty1807046@stud.kuet.ac.bd>
2022-10-30 19:14:33 +0600

    upload product completed

--- app/src/main/java/com/example/kutibari/UploadWorks.java
+++ app/src/main/java/com/example/kutibari/UploadWorks.java
@@ -1,5 +1,6 @@
 package com.example.kutibari;
 
+import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,55 +9,99 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.example.kutibari.databinding.ActivityUploadWorksBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.UUID;
 
 public class UploadWorks extends AppCompatActivity {
-
+    ActivityUploadWorksBinding binding;
     ImageView imageView;
     FloatingActionButton fab;
-
+    EditText editprice,editday,editname;
+    Uri ur;
+    Button uploadworks;
+    private String id,title,price,days;
+    FirebaseDatabase database;
+    FirebaseStorage storage;
+    ActivityResultLauncher<String> launcher ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_upload_works);
+        binding=ActivityUploadWorksBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         imageView=findViewById(R.id.imageforworks);
         fab=findViewById(R.id.floatingActionButton);
+        editprice=findViewById(R.id.editprice);
+        editday=findViewById(R.id.editdays);
+        uploadworks=findViewById(R.id.submitworks);
+        editname=findViewById(R.id.editname);
+        database=FirebaseDatabase.getInstance();
+        storage=FirebaseStorage.getInstance();
+        launcher= registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
+            @Override
+            public void onActivityResult(Uri result) {
+                binding.imageforworks.setImageURI(result);
+                ur=result;
+            }
+        });;
+
         fab.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view)
             {
-                imageChooser();
+                launcher.launch("image/*");
             }
+        });
+        uploadworks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadWork();
+                Intent intent=new Intent(UploadWorks.this,MainActivity.class);
+                startActivity(intent);
+            }
+            
         });
 
     }
-    private void imageChooser()
-    {
-        Intent i = new Intent();
-        i.setType("image/*");
-        i.setAction(Intent.ACTION_GET_CONTENT);
 
-        launchSomeActivity.launch(i);
-    }
-
-    ActivityResultLauncher<Intent> launchSomeActivity
-            = registerForActivityResult(
-            new ActivityResultContracts
-                    .StartActivityForResult(),
-            result -> {
-                if (result.getResultCode()
-                        == Activity.RESULT_OK) {
-                    Intent data = result.getData();
-                    // do your operation from here....
-                    if (data != null
-                            && data.getData() != null) {
-                        Uri selectedImageUri = data.getData();
-                        imageView.setImageURI(selectedImageUri);
+    private void uploadWork() {
+        id= UUID.randomUUID().toString();
+        title=editname.getText().toString();
+        price=editprice.getText().toString();
+        days=editday.getText().toString();
+        final StorageReference reference=storage.getReference().child("image");
+        reference.putFile(ur).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                reference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        Product product=new Product(id,uri.toString(),title,price,days);
+                        database.getReference().child("product").push().setValue(product).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Toast.makeText(getApplicationContext(),"product uploaded",Toast.LENGTH_SHORT);
+                            }
+                        });
                     }
-                }
-            });
+                });
+            }
+        });
+
+
+    }
 }
