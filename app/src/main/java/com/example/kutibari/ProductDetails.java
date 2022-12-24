bfb14dfadb915c99d5528a147462cce26828e79a
Tuna543 <fairuzahmed13579@gmail.com>
2022-12-24 20:54:04 +0600

    trying wishlist

--- app/src/main/java/com/example/kutibari/ProductDetails.java
+++ app/src/main/java/com/example/kutibari/ProductDetails.java
@@ -7,31 +7,35 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.firestore.FirebaseFirestore;
 import com.squareup.picasso.Picasso;
 
 public class ProductDetails extends AppCompatActivity {
     public static boolean running_wishlist_query = false;
-    ImageView pdimage;
-    TextView pdtitle,pdprice,pddays;
+    ImageView pdimage,wishlist;
+    TextView pdtitle,pdprice,pddays,rating_outof,pop5,pop4,pop3,pop2,pop1,avg_rating_text;
     DatabaseReference reference,reference2;
+
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
     String uuid,title,price,pid;
     TextView pdetailtxt;
     TextView wantorder;
     TextView giverating;
-    int finalrate;
+    int finalrate,rate,totalrate,rate1,rate2,rate3,rate4,rate5;
     String stringrate;
+    ProgressBar bar1,bar2,bar3,bar4,bar5;
 
     //rating layout
     private LinearLayout ratenowcontainer;
@@ -41,17 +45,69 @@ public class ProductDetails extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_details);
 
-        FirebaseFirestore firebaseFirestore= FirebaseFirestore.getInstance();
+//        FirebaseFirestore firebaseFirestore= FirebaseFirestore.getInstance();
         pdimage=findViewById(R.id.imageforpdetail);
         pdtitle=findViewById(R.id.namepdetail);
         pdprice=findViewById(R.id.pricepdetail);
         pddays=findViewById(R.id.dayspdetail);
         wantorder=findViewById(R.id.wantorder);
         giverating=findViewById(R.id.yourrating);
+        rating_outof=findViewById(R.id.rating_outof);
+        pop1=findViewById(R.id.pop1);
+        pop2=findViewById(R.id.pop2);
+        pop3=findViewById(R.id.pop3);
+        pop4=findViewById(R.id.pop4);
+        pop5=findViewById(R.id.pop5);
+        avg_rating_text=findViewById(R.id.avg_rating_text);
+        bar1=findViewById(R.id.bar1);
+        bar2=findViewById(R.id.bar2);
+        bar3=findViewById(R.id.bar3);
+        bar4=findViewById(R.id.bar4);
+        bar5=findViewById(R.id.bar5);
+        wishlist=findViewById(R.id.wishlist);
+
         Intent intent=getIntent();
         String uid=intent.getStringExtra("Uid");
 
         uuid=intent.getStringExtra("Uuid");
+
+        reference2= FirebaseDatabase.getInstance().getReference("Wishlist").child("user");
+//        reference2.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                Product product;
+//                for (DataSnapshot dataSnapshot: snapshot.getChildren()){
+//                    String newid=dataSnapshot.getValue(Product.class).getId();
+//                    if(newid.equals(uid)){
+//                        wishlist.setImageTintList(ColorStateList.valueOf(Color.parseColor("#B3261B")));
+//                    }
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
+        wishlist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String puid=mAuth.getCurrentUser().getUid();
+                wishlist.setImageTintList(ColorStateList.valueOf(Color.parseColor("#B3261B")));
+                reference2.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        Toast.makeText(ProductDetails.this,"Product has been added to your wish list", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+            }
+        });
 
         reference= FirebaseDatabase.getInstance().getReference(uuid).child("product");
         reference.addValueEventListener(new ValueEventListener() {
@@ -70,8 +126,63 @@ public class ProductDetails extends AppCompatActivity {
                         title=product.getTitle();
                         price=product.getPrice();
                         pid=product.getId();
+                        totalrate=product.getTotal();
+                        rate1=product.getOne();
+                        rate2=product.getTwo();
+                        rate3=product.getThree();
+                        rate4=product.getFour();
+                        rate5=product.getFive();
+                        rate=0;
+                        if(finalrate==5){
+                            rate=product.getFive();
+                            stringrate="five";
+                        }
+                        else if(finalrate==4){
+                            rate=product.getFour();
+                            stringrate="four";
+                        }
+                        else if(finalrate==3){
+                            rate=product.getThree();
+                            stringrate="three";
+                        }
+                        else if(finalrate==2){
+                            rate=product.getTwo();
+                            stringrate="two";
+                        }
+                        else if(finalrate==1){
+                            rate=product.getOne();
+                            stringrate="one";
+                        }
                     }
                 }
+                float avg=(5*rate5+4*rate4+3*rate3+2*rate2+rate1);
+                if(totalrate==0){
+                    avg=0;
+
+                    bar1.setProgress(0);
+                    bar2.setProgress(0);
+                    bar3.setProgress(0);
+                    bar4.setProgress(0);
+                    bar5.setProgress(0);
+                }
+                else {
+                    avg = avg / totalrate;
+
+                    bar1.setProgress((rate1*100)/totalrate);
+                    bar2.setProgress((rate2*100)/totalrate);
+                    bar3.setProgress((rate3*100)/totalrate);
+                    bar4.setProgress((rate4*100)/totalrate);
+                    bar5.setProgress((rate5*100)/totalrate);
+                }
+//                String s=Integer.toString(totalrate);
+                rating_outof.setText(Integer.toString(totalrate)+" ratings");
+                avg_rating_text.setText(String.valueOf(avg));
+                pop1.setText(Integer.toString(rate1));
+                pop2.setText(Integer.toString(rate2));
+                pop3.setText(Integer.toString(rate3));
+                pop4.setText(Integer.toString(rate4));
+                pop5.setText(Integer.toString(rate5));
+
 
             }
 
@@ -120,29 +231,10 @@ public class ProductDetails extends AppCompatActivity {
                         for (DataSnapshot postSnapshot: snapshot.getChildren()) {
                             String newid=postSnapshot.getValue(Product.class).getId();
                             if (newid.equals(uid)){
-                                int rate=0;
-                                if(finalrate==5){
-                                    rate=product.getFive();
-                                    stringrate="five";
-                                }
-                                else if(finalrate==4){
-                                    rate=product.getFour();
-                                    stringrate="four";
-                                }
-                                else if(finalrate==3){
-                                    rate=product.getThree();
-                                    stringrate="three";
-                                }
-                                else if(finalrate==2){
-                                    rate=product.getTwo();
-                                    stringrate="two";
-                                }
-                                else if(finalrate==1){
-                                    rate=product.getOne();
-                                    stringrate="one";
-                                }
                                 rate += 2;
+                                totalrate+=1;
                                 reference.child(uid).child(stringrate).setValue(rate);
+                                reference.child(uid).child("total").setValue(totalrate);
                                 Toast.makeText(ProductDetails.this,"Your rating has been taken",Toast.LENGTH_SHORT).show();
 
                             }
